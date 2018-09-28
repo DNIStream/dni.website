835f78ea2264244beb9a0ee0187130365ca476ee
Chris Sebok <chris@shadowmoses.co.uk>
2018-09-28 09:03:16 +0100

    Bootstrap packages and powershell script for starting dotnet core app in development

---
+++ run-service-hosts.ps1
@@ -0,0 +1,14 @@
+$root = $PSScriptRoot
+Set-Location $root
+$dotnet = "dotnet.exe"
+# Execute
+$env:ASPNETCORE_URLS = "http://*:12341";
+$env:ASPNETCORE_ENVIRONMENT = "Development";
+$env:DOTNET_USE_POLLING_FILE_WATCHER = "False";
+
+Push-Location src/DNI.API/
+try {
+    & $dotnet @("watch", "run", "-c", "Debug", "--no-launch-profile", "--force")
+} finally {
+    Pop-Location
+}
