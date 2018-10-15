f240ce13d92547c3e6985f6b8c5bec250a378716
Chris Sebok <christopher.sebok@jet2.com>
2018-10-15 20:31:55 +0100

    Fixes #16 - Added GitHub link and refactored social links to use the same component

--- src/DNI.Web/src/app/components/shared/social-links/social-links.component.ts
+++ src/DNI.Web/src/app/components/shared/social-links/social-links.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dni-social-links',
-  templateUrl: './social-links.component.html',
-  styleUrls: ['./social-links.component.scss']
+  templateUrl: './social-links.component.html'
 })
-export class SocialLinksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SocialLinksComponent {
 }
