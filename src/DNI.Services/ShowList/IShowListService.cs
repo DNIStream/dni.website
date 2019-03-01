0022b2901939cad1f9f88eb3c5af6c7564811780
Chris Sebok <chris@shadowmoses.co.uk>
2019-03-01 10:59:44 +0000

    ShowListService order by method implementation #35

--- src/DNI.Services/ShowList/IShowListService.cs
+++ src/DNI.Services/ShowList/IShowListService.cs
@@ -4,5 +4,7 @@ using System.Threading.Tasks;
 namespace DNI.Services.ShowList {
     public interface IShowListService {
         Task<IEnumerable<Show>> GetShowsAsync();
+
+        Task<IEnumerable<Show>> GetShowsAsync(ShowOrderField orderByField, ShowOrderFieldOrder orderByOrder);
     }
 }
--- src/DNI.Services/ShowList/ShowListService.cs
+++ src/DNI.Services/ShowList/ShowListService.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using DNI.Services.Podcast;
 using DNI.Services.Vodcast;
-
-using Google.Apis.Logging;
 
 using Microsoft.Extensions.Logging;
 
@@ -115,6 +114,50 @@ namespace DNI.Services.ShowList {
             return shows?
                 .Where(x => x.Version.HasValue) // Omit results with invalid version strings
                 .OrderByDescending(x => x.Version);
+        }
+
+        /// <summary>
+        ///     Retrieves aggregated vodcast and podcast shows, ordered by the specified field and order.
+        /// </summary>
+        /// <param name="orderByField"></param>
+        /// <param name="orderByOrder"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Show>> GetShowsAsync(ShowOrderField orderByField, ShowOrderFieldOrder orderByOrder) {
+            var shows = await GetShowsAsync();
+
+            // No need to use a reflection based / dynamic implementation as there are
+            // currently only two fields to order by. When new fields are added, this
+            // switch should not need expanding - update the OrderShow() method instead.
+            switch(orderByOrder) {
+                case ShowOrderFieldOrder.Ascending:
+                    shows = shows.OrderBy(s => OrderShow(s, orderByField));
+                    break;
+                case ShowOrderFieldOrder.Descending:
+                    shows = shows.OrderByDescending(s => OrderShow(s, orderByField));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(orderByOrder), orderByOrder, "Specified order not supported");
+            }
+
+            return shows;
+        }
+
+        /// <summary>
+        ///     Basic field selection method for ordering. This could get cumbersome if more fields are introduced, so may be
+        ///     refactored as necessary in the future.
+        /// </summary>
+        /// <param name="show"></param>
+        /// <param name="orderByField"></param>
+        /// <returns></returns>
+        private static object OrderShow(Show show, ShowOrderField orderByField) {
+            switch(orderByField) {
+                case ShowOrderField.PublishedTime:
+                    return show.PublishedTime;
+                case ShowOrderField.Version:
+                    return show.Version;
+                default:
+                    return null;
+            }
         }
 
         #region Helpers
