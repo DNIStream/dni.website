2ff52c74f3161b41f0518c789600415860252e58
Chris Sebok <chris@shadowmoses.co.uk>
2019-05-26 22:31:49 +0100

    Fixed Show Archive page bugs

--- src/DNI.Web/src/app/components/pages/show-archive/show-archive.component.spec.ts
+++ src/DNI.Web/src/app/components/pages/show-archive/show-archive.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 import { CollapseModule } from 'ngx-bootstrap';
 
@@ -22,6 +23,7 @@ describe('ShowArchiveComponent', () => {
       imports: [
         RouterTestingModule,
         CollapseModule,
+        FormsModule,
         HttpClientTestingModule
       ]
     })
