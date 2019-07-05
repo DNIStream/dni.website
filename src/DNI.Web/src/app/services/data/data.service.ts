9647da3714a4c35104d052b063256bd2919f777e
Chris Sebok <chris@shadowmoses.co.uk>
2019-07-05 15:31:44 +0100

    Resolved #48 added new drop down menu link list for podcast subscription options

--- src/DNI.Web/src/app/services/data/data.service.ts
+++ src/DNI.Web/src/app/services/data/data.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import { Creator } from 'app/model/creator';
 import { environment } from 'environments/environment';
+import { DiscoveryPlatform } from 'app/model/discovery-platform';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,10 @@ export class DataService {
   public creators(): Observable<Creator[]> {
     const uri = environment.webUri + 'assets/data/creators.json';
     return this.http.get<Creator[]>(uri);
+  }
+
+  public discoveryPlatforms(): Observable<DiscoveryPlatform[]> {
+    const uri = environment.webUri + 'assets/data/discovery-platforms.json';
+    return this.http.get<DiscoveryPlatform[]>(uri);
   }
 }
