7ec596317d9c3dfc6cfca9b811854ec754878e6c
Chris Sebok <chris@shadowmoses.co.uk>
2020-03-07 15:40:15 +0000

    Updating environment variable configuration for docker and docker compose

--- setup-env-vars.ps1
+++ setup-env-vars.ps1
@@ -1,4 +1,3 @@
-#param([string]$file = "docker-envs\dev.env")
 param([string]$file = ".env")
 $ErrorActionPreference = "Stop"
 
@@ -15,13 +14,15 @@ $vars | % {
 }
 
 # DotNet Core Env Variable overrides. Out-Null for safe streaming.
-New-Item env:\CAPTCHA__SecretKey -Value $Env:CAPTCHA_KEY -Force | Out-Null
-New-Item env:\General__SmtpServer -Value $Env:SMTP_SERVER -Force | Out-Null
+New-Item env:\CAPTCHA__SecretKey -Value "$Env:CAPTCHA_KEY" -Force | Out-Null
+New-Item env:\General__SmtpServer -Value "$Env:SMTP_SERVER" -Force | Out-Null
+New-Item env:\General__SmtpServerPort -Value "$Env:SMTP_PORT" -Force | Out-Null
+New-Item env:\General__SmtpEnableSSL -Value "$Env:SMTP_ENABLE_SSL" -Force | Out-Null
 New-Item env:\General__SmtpUsername -Value "$Env:SMTP_USERNAME" -Force | Out-Null
 New-Item env:\General__SmtpPassword -Value "$Env:SMTP_PASSWORD" -Force | Out-Null
-New-Item env:\General__ErrorEmailFrom -Value $Env:ERROR_EMAIL_FROM -Force | Out-Null
-New-Item env:\General__ErrorEmailTo -Value $Env:ERROR_EMAIL_TO -Force | Out-Null
-New-Item env:\General__ContactEmailTo -Value $Env:CONTACT_EMAIL_TO -Force | Out-Null
+New-Item env:\General__ErrorEmailFrom -Value "$Env:ERROR_EMAIL_FROM" -Force | Out-Null
+New-Item env:\General__ErrorEmailTo -Value "$Env:ERROR_EMAIL_TO" -Force | Out-Null
+New-Item env:\General__ContactEmailTo -Value "$Env:CONTACT_EMAIL_TO" -Force | Out-Null
 
 Write-Host "======================================================="
 Write-Host "="
--- src/DNI.Web/src/environments/environment.prod.ts
+++ src/DNI.Web/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  webUri: 'https://www.dnistream.live/',
-  // webUri: 'http://localhost:4000/',
-  apiBaseUri: 'https://api.dnistream.live/',
-  // apiBaseUri: 'http://localhost:12341/',
+  // webUri: 'https://www.dnistream.live/',
+  webUri: 'http://localhost:4200/',
+  // apiBaseUri: 'https://api.dnistream.live/',
+  apiBaseUri: 'http://localhost:12341/',
   recaptchaSiteKey: '6Lc2_XMUAAAAAOd_v5nbtAE_rxgbqspCJJpYQ9zn',
   version: '1.3.2',
   versionText: 'The Left-Hoofed Antelope'
