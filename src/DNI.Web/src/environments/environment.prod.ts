61268ba45a1851858725cec0baa945b17f533c2e
Chris Sebok <chris@shadowmoses.co.uk>
2020-03-07 16:17:33 +0000

    WIP #62 Angular production file update for deployment

--- src/DNI.Web/src/environments/environment.prod.ts
+++ src/DNI.Web/src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
   production: true,
-  // webUri: 'https://www.dnistream.live/',
-  webUri: 'http://localhost:4200/',
-  // apiBaseUri: 'https://api.dnistream.live/',
-  apiBaseUri: 'http://localhost:12341/',
+  webUri: 'https://www.dnistream.live/',
+  apiBaseUri: 'https://api.dnistream.live/',
   recaptchaSiteKey: '6Lc2_XMUAAAAAOd_v5nbtAE_rxgbqspCJJpYQ9zn',
+  // Local dev production testing
+  // webUri: 'http://localhost:4200/',
+  // apiBaseUri: 'http://localhost:12341/',
+  // recaptchaSiteKey: '6Lcv_3MUAAAAAPTlA1F3Jk2X3mc_Vnixqbgk3pgv',
   version: '1.3.2',
   versionText: 'The Left-Hoofed Antelope'
 };
