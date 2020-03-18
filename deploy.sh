488bdba16f058c213ab94555ced5922441ba3ff9
Chris Sebok <chris@shadowmoses.co.uk>
2020-03-18 17:33:12 +0000

    Travis testing

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo Starting deploy script
 #if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_PULL_REQUEST;
     echo $TRAVIS_BRANCH;
-    version=line=$(head -n 1 VERSION);
+    version=$(head -n 1 VERSION);
     echo $version;
     docker image ls;
     # TODO: Tag dni docker images
