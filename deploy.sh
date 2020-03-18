5c3207dadef1d7626b571337dded0e7de65bd30a
Chris Sebok <chris@shadowmoses.co.uk>
2020-03-18 17:09:49 +0000

    Read version file

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ echo Starting deploy script
 #if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_PULL_REQUEST;
     echo $TRAVIS_BRANCH;
-    echo $TRAVIS_TAG;
+    $version = line=$(head -n 1 VERSION);
+    echo $version;
+    docker image ls;
 #; fi
