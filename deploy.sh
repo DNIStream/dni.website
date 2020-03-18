3dd47abe9d89ff8fa91611bacc9e9c6f3afef15d
Chris Sebok <chris@shadowmoses.co.uk>
2020-03-18 17:20:00 +0000

    TODOs for CD and version variable set fix

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,15 @@ echo Starting deploy script
 #if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_PULL_REQUEST;
     echo $TRAVIS_BRANCH;
-    $version = line=$(head -n 1 VERSION);
+    version=line=$(head -n 1 VERSION);
     echo $version;
     docker image ls;
+    # TODO: Tag dni docker images
+    # TODO: Auth with docker hub
+    # TODO: Push to docker hub
+    # TODO: Log on to DO box
+    # TODO: Pull docker hub images
+    # TODO: Potentially copy NGINX files
+    # TODO: Run images on DO box
+    # TODO: Clean up old images?
 #; fi
