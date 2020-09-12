e234c3261c36fe4b7f8c07faacd0d831d649c6de
Chris Sebok <chris@shadowmoses.co.uk>
2020-09-12 16:20:00 +0100

    Version 2.0.6

--- src/DNI.Web/src/environments/environment.prod.ts
+++ src/DNI.Web/src/environments/environment.prod.ts
@@ -7,9 +7,10 @@ export const environment = {
   webUri: 'https://www.dnistream.live/',
   apiBaseUri: 'https://api.dnistream.live/',
   recaptchaSiteKey: '6Lc2_XMUAAAAAOd_v5nbtAE_rxgbqspCJJpYQ9zn',
-  version: '2.0.5',
+  version: '2.0.6',
   versionText: 'The Jolly Guinea Pig'
 };
+
 
 
 
